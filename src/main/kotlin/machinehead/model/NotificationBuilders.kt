8b2a473059ae7d46c21e3137b2ff34d03e92ac6b
Spasikov Nikola <nikola.spasikov@sbb.ch>
2020-02-23 11:58:49 +0100

    - payload builder with custom fields and notification builders (•̀ᴗ•́)و

---
+++ src/main/kotlin/machinehead/model/NotificationBuilders.kt
@@ -0,0 +1,56 @@
+package machinehead.model
+
+fun payload(block: PayloadBuilder.() -> Unit): Payload = PayloadBuilder().apply(block).build()
+
+class PayloadBuilder {
+    var notification: Notification? = null
+    var custom = hashMapOf<String, Any>()
+
+    fun notification(block: NotificationBuilder.() -> Unit) {
+        notification = NotificationBuilder().apply(block).build()
+    }
+
+    fun build(): Payload = Payload(notification, custom)
+}
+
+class NotificationBuilder {
+    var aps: Aps? = null
+
+    fun aps(block: ApsBuilder.() -> Unit) {
+        aps = ApsBuilder().apply(block).build()
+    }
+
+    fun build(): Notification = Notification(aps)
+}
+
+class ApsBuilder {
+
+    var alert: Alert? = null
+    var sound: String? = null
+    var badge: Int? = null
+    var category: String? = null
+    var thread_id: String? = null
+    var mutable_content: Int? = null
+    var content_available: Int? = null
+    var target_content_id: String? = null
+
+    fun alert(block: AlertBuilder.() -> Unit) {
+        alert = AlertBuilder().apply(block).build()
+    }
+
+    fun build(): Aps =
+        Aps(alert, sound, badge, category, thread_id, mutable_content, content_available, target_content_id)
+}
+
+class AlertBuilder {
+    var body: String? = null
+    var title: String? = null
+    var subtitle: String? = null
+    var loc_key: String? = null
+    var launch_image: String? = null
+    var title_loc_key: String? = null
+    var subtitle_loc_key: String? = null
+
+    fun build(): Alert = Alert(body, title, subtitle, loc_key, launch_image, title_loc_key, subtitle_loc_key)
+}
+
---
+++ src/main/kotlin/machinehead/model/NotificationModels.kt
@@ -0,0 +1,28 @@
+package machinehead.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Payload(var notification: Notification? = null, var custom: HashMap<String, Any>? = null)
+
+data class Notification(var aps: Aps? = null)
+
+data class Aps(
+    val alert: Alert? = null,
+    var sound: String? = null,
+    var badge: Int? = null,
+    var category: String? = null,
+    @SerializedName("thread-id") var thread_id: String? = null,
+    @SerializedName("mutable-content") var mutable_content: Int? = null,
+    @SerializedName("content-available") var content_available: Int? = null,
+    @SerializedName("target-content-id") var target_content_id: String? = null
+)
+
+data class Alert(
+    var body: String? = null,
+    var title: String? = null,
+    var subtitle: String? = null,
+    @SerializedName("loc-key") var loc_key: String? = null,
+    @SerializedName("launch-image") var launch_image: String? = null,
+    @SerializedName("title-loc-key") var title_loc_key: String? = null,
+    @SerializedName("subtitle-loc-key") var subtitle_loc_key: String? = null
+)
