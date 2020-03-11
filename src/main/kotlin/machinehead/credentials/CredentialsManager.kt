d457e82fb8c2724a29906e9d93898a2b9b85c4f3
Spasikov Nikola <nikola.spasikov@sbb.ch>
2020-03-11 16:43:24 +0100

    read the credentials from the env variable password and cretificate (•̀ᴗ•́)و

--- src/main/kotlin/machinehead/credentials/CredentialsManager.kt
+++ src/main/kotlin/machinehead/credentials/CredentialsManager.kt
@@ -1,6 +1,7 @@
 package machinehead.credentials
 
 import machinehead.result.PushResult
+import machinehead.result.Response
 import javax.net.ssl.SSLSocketFactory
 import javax.net.ssl.X509TrustManager
 
@@ -18,9 +19,27 @@ class P12CredentialsFromEnv : CredentialsManager {
     ): PushResult {
         val sslFactory: SSLSocketFactory? = null
         val trustManager: X509TrustManager? = null
-        if (sslFactory == null || trustManager == null) {
-            return onError()
+        return readFromEnv() { certificate, password ->
+            if (sslFactory == null || trustManager == null) {
+                return@readFromEnv onError()
+            }
+            return@readFromEnv onCredentials(sslFactory, trustManager)
         }
-        return onCredentials(sslFactory, trustManager)
+    }
+
+    private fun readFromEnv(credentials: (String, String) -> PushResult): PushResult {
+        val pass = System.getenv("PASSWORD")
+        val cert = System.getenv("CERTIFICATE")
+        if (pass == null || cert == null) {
+            return PushResult(
+                hashMapOf(
+                    "error" to Response(
+                        "500",
+                        "Could not find credentials in environment variables"
+                    )
+                )
+            )
+        }
+        return credentials(cert, cert)
     }
 }
