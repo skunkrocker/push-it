262dfecd055ff172fef50acca64e0b1d5d49c27a
Spasikov Nikola <nikola.spasikov@sbb.ch>
2020-06-03 20:06:46 +0200

    - add headers to each request (•̀ᴗ•́)و

--- src/main/kotlin/machinehead/okclient/OkClientWithCredentials.kt
+++ src/main/kotlin/machinehead/okclient/OkClientWithCredentials.kt
@@ -5,6 +5,8 @@ import machinehead.ClientError
 import machinehead.model.Payload
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
 import javax.net.ssl.SSLSocketFactory
 import javax.net.ssl.X509TrustManager
 
@@ -17,23 +19,30 @@ class OkClientWithCredentials {
             try {
                 val okClientBuilder = OkHttpClient().newBuilder()
                 okClientBuilder.sslSocketFactory(credentials.first, credentials.second)
-                okClientBuilder.addInterceptor { chain: Interceptor.Chain ->
-                    val original = chain.request()
-                    val responseBuilder = original.newBuilder()
-                    //TODO add the headers
-
-                    chain.proceed(
-                        responseBuilder
-                            .method(original.method, original.body)
-                            .build()
-                    )
-                }
+                okClientBuilder.addInterceptor(addHeadersToCurrentRequest(payload))
                 val okClient = okClientBuilder.build()
                 return Either.right(okClient)
             } catch (e: Exception) {
                 println("Could not create ok client")
             }
             return Either.left(ClientError("could not create http client"))
+        }
+
+        private fun addHeadersToCurrentRequest(payload: Payload): (Interceptor.Chain) -> Response {
+            return { chain: Interceptor.Chain ->
+                val original: Request = chain.request()
+                val responseBuilder: Request.Builder = original.newBuilder()
+
+                payload.headers.forEach() {
+                    responseBuilder.addHeader(it.key, it.value.toString())
+                }
+
+                val request = responseBuilder
+                    .method(original.method, original.body)
+                    .build()
+
+                chain.proceed(request)
+            }
         }
     }
 }
