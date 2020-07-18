c97154e0234ea1396b05586142fd96afe798be47
Spasikov Nikola <spasikov@yahoo.de>
2020-07-18 14:49:13 +0200

    - remove the unneeded life cycle (•̀ᴗ•́)و ̑̑

--- src/test/kotlin/machinehead/push/MissingTopicTest.kt
+++ src/test/kotlin/machinehead/push/MissingTopicTest.kt
@@ -3,16 +3,11 @@ package machinehead.push
 import machinehead.PushIt
 import machinehead.push.TestData.Companion.TOKEN
 import machinehead.push.TestData.Companion.`get test payload`
-import org.junit.jupiter.api.AfterAll
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestInstance
-import org.junit.jupiter.api.TestInstance.Lifecycle
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
 
-@TestInstance(Lifecycle.PER_CLASS)
 class MissingTopicTest {
 
     @Test
