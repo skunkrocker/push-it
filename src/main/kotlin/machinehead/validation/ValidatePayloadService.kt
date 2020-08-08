656eb7ae546b035a8b57cd44ec9f4cc83f4599e5
Spasikov Nikola <spasikov@yahoo.de>
2020-08-08 18:53:25 +0200

    validate payload service impl (•̀ᴗ•́)و ̑̑

--- src/main/kotlin/machinehead/validation/ValidatePayloadService.kt
+++ src/main/kotlin/machinehead/validation/ValidatePayloadService.kt
@@ -2,15 +2,33 @@ package machinehead.validation
 
 import arrow.core.None
 import arrow.core.Option
+import arrow.core.Some
 import machinehead.model.ClientError
 import machinehead.model.Payload
+import machinehead.model.yaml.From
+import machinehead.model.yaml.YAMLFile
+import machinehead.okclient.PayloadValidator
+import machinehead.parse.ParseErrors
 
 interface ValidatePayloadService {
     fun isValid(payload: Payload): Option<ClientError>
 }
 
 class ValidatePayloadServiceImpl : ValidatePayloadService {
+
+    private var errorMessages: ParseErrors = From the YAMLFile("payload-errors.yml", ParseErrors::class)
+
     override fun isValid(payload: Payload): Option<ClientError> {
+        if (payload.tokens.isEmpty()) {
+            return Some(ClientError(errorMessages.noTokens.orEmpty()))
+        }
+        if (payload.notification?.aps?.alert == null) {
+            return Some(ClientError(errorMessages.noAlert.orEmpty()))
+        }
+
+        if (payload.headers.isEmpty()) {
+            return Some(ClientError(errorMessages.noTopic.orEmpty()))
+        }
         return None
     }
 }
