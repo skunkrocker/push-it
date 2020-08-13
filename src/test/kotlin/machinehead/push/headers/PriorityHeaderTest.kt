0d386885915740b3c642b5a68c0f417f6d997901
Spasikov Nikola <spasikov@yahoo.de>
2020-08-13 18:25:27 +0200

    - old way method removed from last headers test (•̀ᴗ•́)و ̑̑

--- src/test/kotlin/machinehead/push/headers/PriorityHeaderTest.kt
+++ src/test/kotlin/machinehead/push/headers/PriorityHeaderTest.kt
@@ -1,7 +1,7 @@
 package machinehead.push.headers
 
 import com.squareup.okhttp.mockwebserver.MockWebServer
-import machinehead.PushIt
+import machinehead.extensions.pushIt
 import machinehead.okclient.OkClientAPNSRequest
 import machinehead.push.responses.APNSHeaders.Companion.APNS_PRIORITY
 import machinehead.push.assertion.APNSResponseAssertion
@@ -11,10 +11,7 @@ import machinehead.push.TestData.Companion.APNS_TOPIC_VALUE
 import machinehead.push.TestData.Companion.TOKEN
 import machinehead.push.TestData.Companion.`get test payload`
 import machinehead.push.responses.APNSHeaderResponses
-import org.junit.jupiter.api.AfterAll
-import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.*
 import org.junit.jupiter.api.TestInstance.Lifecycle
 
 @TestInstance(Lifecycle.PER_CLASS)
@@ -35,23 +32,25 @@ class PriorityHeaderTest : APNSResponseAssertion() {
     @Test
     @ExperimentalStdlibApi
     fun `priority header has invalid value - report apns message`() {
-        //given
-        val payload = `get test payload`(
+        `get test payload`(
             TOKEN, hashMapOf(
                 APNS_TOPIC_KEY to APNS_TOPIC_VALUE,
                 APNS_PRIORITY to "11"
             )
         )
-        //when
-        val pushIt = PushIt()
-        pushIt.with(payload)
+            .pushIt { either ->
+                mockWebServer.takeRequest()
+                either.fold(
+                    {
+                        Assertions.assertFalse(true)
+                    },
+                    { errorsAndResults ->
 
-        //then
-        mockWebServer.takeRequest()
-
-        val platformResponses = pushIt.platformResponseListener.platformResponses
-
-        assertResponse(platformResponses, "bad_priority.json")
+                        val platformResponses = errorsAndResults.results
+                        assertResponse(platformResponses, "bad_priority.json")
+                    }
+                )
+            }
     }
 
     @AfterAll
